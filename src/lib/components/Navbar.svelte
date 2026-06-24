636c44f5a08c4e76f1b1f8964b2df1456b930152
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-06-24 21:46:19 +0600

    Setup project, make navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<div class="flex items-center h-15 font-display pl-15 pr-15 bg-bg-primary">
+    <h1 class="flex items-baseline pr-2 text-text-primary text-[130%] tracking-widest">
+        FYNR1X<span class="text-accent-red ml-1 mr-1">.</span>
+        <span class="text-text-muted font-body text-[40%] tracking-normal">
+            @faizeenhoque
+        </span>
+    </h1>
+
+    <ul class="flex items-center ml-auto gap-6">
+        <li class="m-2 font-body text-text-muted text-[60%] hover:text-accent-red hover:cursor-pointer">ABOUT</li>
+        <li class="m-2 font-body text-text-muted text-[60%] hover:text-accent-red hover:cursor-pointer">PROJECTS</li>
+        <li class="m-2 font-body text-text-muted text-[60%] hover:text-accent-red hover:cursor-pointer">STACK</li>
+        <button class="m-2 p-2 font-body text-text-muted text-[60%] border border-accent-red hover:bg-accent-red hover:text-text-primary cursor-pointer">FIND ME</button>
+    </ul>
+</div>
